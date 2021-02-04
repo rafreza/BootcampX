4d794fe887608293b6cc4d72f9434773967ee8f9
Rafin Reza <rafintokyo@gmail.com>
2021-02-04 20:13:42 +0000

    modify queries to be parameterized

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
+/*
 // SQL data as JS array
 pool.query(`
 SELECT id, name, cohort_id
@@ -30,8 +30,9 @@ LIMIT 5;
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
 });
-
+*/
 // process.argv injection
+/*
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
@@ -40,6 +41,28 @@ WHERE cohorts.name LIKE '%${process.argv[2]}%'
 LIMIT ${process.argv[3] || 5};
 `)
 .then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
+*/
+
+//Injection proof
+
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values).then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
